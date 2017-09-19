7b3e83a1f7ecf272127f95bc598a095d06278619
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2017-09-19 13:50:02 +0200

    update regular event

--- RegularEvent.cpp
+++ RegularEvent.cpp
@@ -1,1 +1,54 @@
 #include "RegularEvent.h"
+    
+void RegularEvent::update() {		
+	engine.globalObject().setProperty("elapsed", start.secsTo(QTime::currentTime()));
+	QScriptValue result = engine.evaluate(contents, fileName);
+	
+	if (result.isError()) {
+		qDebug()<<
+							  QString::fromLatin1("%0:%1: %2")
+							  .arg(fileName)
+							  .arg(result.property("lineNumber").toInt32())
+							  .arg(result.toString());
+		timer->stop();
+		return;
+	}
+	else if(result.isBool() && result.toBool())
+		stop();
+}
+	
+void RegularEvent::loadUpdate()
+{
+    int curLap;
+
+    // the period between loadUpdate calls is not constant, and not exactly LOADRATE,
+    // therefore, use a QTime timer to measure the load period
+    load_msecs += load_period.restart();
+
+    //if (status&RT_MODE_ERGO) {
+        long load = ergFile->wattsAt(load_msecs, curLap);
+
+        // we got to the end!
+        if (load == -100) {
+            stop();
+        } else {
+            emit setLoad(load);
+        }
+    /*} else {
+        slope = ergFile->gradientAt(displayWorkoutDistance*1000, curLap);
+
+        if(displayWorkoutLap != curLap)
+        {
+            context->notifyNewLap();
+        }
+        displayWorkoutLap = curLap;
+
+        // we got to the end!
+        if (slope == -100) {
+            Stop(DEVICE_OK);
+        } else {
+            foreach(int dev, activeDevices) Devices[dev].controller->setGradient(slope);
+            context->notifySetNow(displayWorkoutDistance * 1000);
+        }
+    }*/
+}
--- RegularEvent.h
+++ RegularEvent.h
@@ -1,17 +1,25 @@
 #pragma once
 #include <QtScript/QtScript>
+#include "Train/ErgFile.h"
+
+#define LOADRATE       1000 // rate at which load is adjusted
+
 
 class RegularEvent : public QObject
 {
     Q_OBJECT
     
     QScriptEngine &engine;
-    QTimer *timer;
+    QTimer *timer, *load_timer;
     QString fileName;
     QString contents;
+    QTime start;
+    QTime load_period;
+    ErgFile *ergFile;
+    long load_msecs;
 public:
-    RegularEvent(QString filename, QScriptEngine &engine, const int interval) :
-    engine(engine), fileName(filename)
+    RegularEvent(QString filename, QScriptEngine &engine, const int interval, ErgFile *erg=NULL) :
+    engine(engine), fileName(filename), start(QTime::currentTime()), ergFile(erg), load_msecs(0)
     {
 		QFile scriptFile(fileName);
 		scriptFile.open(QIODevice::ReadOnly);
@@ -20,22 +28,26 @@ public:
 		scriptFile.close();
 		
 		timer = new QTimer(this);
+		load_timer = new QTimer(this);
+		
 		connect(timer, SIGNAL(timeout()), this, SLOT(update()));
+		connect(load_timer, SIGNAL(timeout()), this, SLOT(loadUpdate()));
+		
 		timer->start(interval);
+		if(ergFile)
+			load_timer->start(LOADRATE);
+	}
+	
+	void stop() {
+		timer->stop();
+		load_timer->stop();
+		QCoreApplication::exit(0);
 	}
 
+signals:
+	void setLoad(double);
+        
 public slots:
-    void update() {		
-		QScriptValue result = engine.evaluate(contents, fileName);
-		
-		if (result.isError()) {
-			qDebug()<<
-								  QString::fromLatin1("%0:%1: %2")
-								  .arg(fileName)
-								  .arg(result.property("lineNumber").toInt32())
-								  .arg(result.toString());
-			timer->stop();
-			return;
-		}
-	}
+    void update();
+    void loadUpdate();
 };
